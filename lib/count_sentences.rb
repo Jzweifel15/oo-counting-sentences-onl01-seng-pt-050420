b2070755d49a437e3a5462cafa49296a020d9a93
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-19 21:59:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(".", "?", "!").reject {|sentence| sentence.empty?}.count
+    self.split(".", "?", "!").reject {|sentence| sentence.empty?}.size
   end
   
 end
